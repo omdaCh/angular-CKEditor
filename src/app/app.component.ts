13f676e0ab209c2e97a2c0818a585262b26da3eb
chemakhi imad <chemakhi.imad@gmail.com>
2024-12-02 11:05:28 +0100

    simple version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// app.component.ts
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import {  Bold, Essentials, Italic, Paragraph, Undo, DecoupledEditor, List, ClassicEditor } from 'ckeditor5';
+
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    imports: [ CKEditorModule ],
+    standalone: true
+} )
 export class AppComponent {
-  title = 'angular-CKEditor';
+    title = 'angular';
+
+    public Editor = ClassicEditor;
+    public config = {
+        plugins: [ Bold, Essentials, Italic, Paragraph, Undo, List  ],
+        toolbar: [ 'undo', 'redo', '|', 'bold', 'italic' , 'bulletedList']
+    }
 }
