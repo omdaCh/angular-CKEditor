980b81b2184aef350ea14b5dec450b53dec0e9c5
chemakhi imad <chemakhi.imad@gmail.com>
2024-12-03 15:06:45 +0100

    before upgrading angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 // app.component.ts
 
 import { Component, ViewEncapsulation } from '@angular/core';
-import { ChangeEvent, CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { Bold, Essentials, Italic, Paragraph, Undo, List, ClassicEditor } from 'ckeditor5';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { Bold, Essentials, Italic, Paragraph, Undo, List, ClassicEditor, Heading } from 'ckeditor5';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import coreTranslations from 'ckeditor5/translations/ar.js';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    imports: [CKEditorModule, MatButtonModule,FormsModule ],
+    imports: [CKEditorModule, FormsModule, MatFormFieldModule, MatButtonModule, MatCheckboxModule, MatSelectModule],
     standalone: true
 })
 export class AppComponent {
@@ -19,14 +23,32 @@ export class AppComponent {
 
     public isDisabled: boolean = false;
     public isRtl: boolean = false;
+    public selectedLanguage: 'English' | 'Arabic' = 'English';
+    public isCustomTheme: boolean = false;
+
     public showEditor: boolean = true;
     public editorData: string = '<p>Hello, world!</p>';
 
     public editor = ClassicEditor;
     public config = {
-        plugins: [Bold, Essentials, Italic, Paragraph, Undo, List],
-        toolbar: ['undo', 'redo', '|', 'bold', 'italic', 'bulletedList'],
-        language: 'fr'
+        plugins: [Bold, Essentials, Italic, Paragraph, Undo, List, Heading],
+        toolbar: ['undo', 'redo', '|', 'bold', 'italic', 'bulletedList', 'heading'],
+        language: 'ar',
+        placeholder: 'Type the content here!',
+        translations: [coreTranslations],
+
+    }
+
+    constructor() {
+        // if (this.editor.defaultConfig) {
+        //     this.editor.defaultConfig.language = 'ar'; // Set default language
+        //     // this.editor.defaultConfig.translations = {
+        //     //     ...this.editor.defaultConfig.translations,
+        //     //     ar: coreTranslations,
+        //     // };
+        // } else {
+        //     console.error('CKEditor defaultConfig is not defined.');
+        // }
     }
 
 
@@ -37,15 +59,18 @@ export class AppComponent {
     toggleEditorDirection() {
         this.isRtl = !this.isRtl;
         this.showEditor = false;
-        this.editorData 
+        this.editorData
         setTimeout(() => {
             this.config = {
                 ...this.config,
-                language: this.isRtl ? 'ar' : 'fr'
+                language: this.isRtl ? 'ar' : 'fr',
+                // translations: [
+                //     coreTranslations
+                // ]
             };
             this.showEditor = true;
         }, 100)
     }
 
-    
+
 }
