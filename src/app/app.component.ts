d6cd321e1f58aece64ce2080bc1729abb056ef7b
chemakhi imad <chemakhi.imad@gmail.com>
2024-12-08 13:50:51 +0100

    language and custom theming added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,39 @@
-// app.component.ts
-
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { Bold, Essentials, Italic, Paragraph, Undo, List, ClassicEditor, Heading } from 'ckeditor5';
+import {
+    Bold, Essentials, Italic, Paragraph, Undo, List, ClassicEditor, Heading, Link, Table, Image,
+    ImageCaption,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Base64UploadAdapter,
+    Underline,
+    Indent,
+} from 'ckeditor5';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import arTranslations from 'ckeditor5/translations/ar.js';
+import enTranslations from 'ckeditor5/translations/en.js';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    encapsulation: ViewEncapsulation.None, 
-    imports: [CKEditorModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatMenuModule, MatCheckboxModule, MatSelectModule,MatIconModule]
+    encapsulation: ViewEncapsulation.None,
+    imports: [CKEditorModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatMenuModule, MatCheckboxModule, MatSelectModule, MatIconModule]
 })
 export class AppComponent {
     title = 'angular';
 
     public isDisabled: boolean = false;
     public isRtl: boolean = false;
-    public selectedLanguage: 'English' | 'Arabic' = 'English';
+    public editorLanguage: 'en' | 'ar' = 'en';
     public isCustomTheme: boolean = false;
 
     public showEditor: boolean = true;
@@ -31,45 +42,72 @@ export class AppComponent {
     public editor = ClassicEditor;
     public config = {
         licenseKey: 'GPL',
-        plugins: [Bold, Essentials, Italic, Paragraph, Undo, List, Heading],
-        toolbar: ['undo', 'redo', '|', 'bold', 'italic', 'bulletedList', 'heading'],
-        language: 'ar',
+        plugins: [Bold, 
+            Essentials, 
+            Italic, 
+            Underline, 
+            Paragraph, 
+            Undo, 
+            List, 
+            Indent,
+			// IndentBlock,
+            Heading, 
+            Link, 
+            Table, 
+            ImageUpload, 
+            Image, 
+            ImageCaption,
+            ImageResize,
+            ImageStyle,
+            ImageToolbar,
+            ImageUpload,
+            Base64UploadAdapter],
+
+        toolbar: ['undo', 
+            'redo', 
+            '|', 
+            'heading', 
+            '|', 
+            'bold', 
+            'italic', 
+            'underline', 
+            '|', 
+            'link', 
+            'insertTable', 
+            'uploadImage', 
+            '|', 
+            'bulletedList', 
+            'numberedList',
+            'outdent',
+            'indent'],
+        language: 'en',
         placeholder: 'Type the content here!',
-        // translations: [coreTranslations],
-
+        translations: [enTranslations]
     }
 
-    constructor() {
-        // if (this.editor.defaultConfig) {
-        //     this.editor.defaultConfig.language = 'ar'; // Set default language
-        //     // this.editor.defaultConfig.translations = {
-        //     //     ...this.editor.defaultConfig.translations,
-        //     //     ar: coreTranslations,
-        //     // };
-        // } else {
-        //     console.error('CKEditor defaultConfig is not defined.');
-        // }
-    }
+    constructor() { }
 
 
     toggleDisabled() {
         this.isDisabled = !this.isDisabled
     }
 
-    toggleEditorDirection() {
-        this.isRtl = !this.isRtl;
+    setLanguage(language: 'en' | 'ar') {
         this.showEditor = false;
-        this.editorData
-        setTimeout(() => {
-            this.config = {
-                ...this.config,
-                language: this.isRtl ? 'ar' : 'fr',
-                // translations: [
-                //     coreTranslations
-                // ]
-            };
-            this.showEditor = true;
-        }, 100)
+        if (language == this.editorLanguage)
+            return;
+        else {
+            this.editorLanguage = language;
+            setTimeout(() => {
+                this.config = {
+                    ...this.config,
+                    language: this.editorLanguage,
+                    translations: [this.editorLanguage == 'en' ? enTranslations : arTranslations]
+                };
+                this.showEditor = true;
+            }, 100);
+        }
+
     }
 
 
